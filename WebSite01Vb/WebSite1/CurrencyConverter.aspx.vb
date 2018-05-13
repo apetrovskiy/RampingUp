47fc50066343a6dfe0c5c071723f825af4f5a48f
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-05-13 19:15:38 +0300

    .

--- WebSite01Vb/WebSite1/CurrencyConverter.aspx.vb
+++ WebSite01Vb/WebSite1/CurrencyConverter.aspx.vb
@@ -3,17 +3,26 @@ Partial Class CurrencyConverter
     Inherits System.Web.UI.Page
 
     Protected Sub Convert_ServerClick(ByVal sender As Object, ByVal d As System.EventArgs) Handles Convert.ServerClick
-        Dim USAmount As Double = Val(US.value)
-        Dim EuroAmount As Double = USAmount * 0.85
-        Result.InnerText = USAmount.ToString() & " U.S. dollars = "
-        Result.InnerText &= EuroAmount.ToString() & " Euros."
+        'Dim USAmount As Double = Val(US.value)
+        'Dim EuroAmount As Double = USAmount * 0.85
+        'Result.InnerText = USAmount.ToString() & " U.S. dollars = "
+        'Result.InnerText &= EuroAmount.ToString() & " Euros."
+
+        Dim oldAmount As Double = Val(US.Value)
+
+        ' Retrieve the selected ListItem object by its index number.
+        Dim item As ListItem = Currency.Items(Currency.SelectedIndex)
+
+        Dim newAmount As Double = oldAmount * Val(item.Value)
+        Result.InnerText = oldAmount.ToString() & " U.S. dollars = "
+        Result.InnerText &= newAmount.ToString() & " " & item.Text
     End Sub
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Me.IsPostBack = False Then
-            Currency.Items.Add("Euro")
-            Currency.Items.Add("Japanese Yen")
-            Currency.Items.Add("Canadian Dollar")
+            Currency.Items.Add(New ListItem("Euros", "0.85"))
+            Currency.Items.Add(New ListItem("Japanese Yen", "110.33"))
+            Currency.Items.Add(New ListItem("Canadian Dollars", "1.2"))
         End If
     End Sub
 End Class
