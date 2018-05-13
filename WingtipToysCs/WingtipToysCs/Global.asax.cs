4f4ba0c0d20b3d59100cff6f6ec791627c53cefd
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-05-13 23:47:33 +0300

    .

---
+++ WingtipToysCs/WingtipToysCs/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿namespace WingtipToysCs
+{
+    using System;
+    using System.Data.Entity;
+    using System.Web;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+    using Models;
+
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database.
+            Database.SetInitializer(new ProductDatabaseInitializer());
+        }
+    }
+}
---
+++ WingtipToysCs/WingtipToysCs/Models/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿namespace WingtipToysCs.Models
+{
+    using System.Data.Entity;
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("WingtipToysCs")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
